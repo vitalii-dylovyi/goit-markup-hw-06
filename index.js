e06ec4e456baf3891bd84f4d43cbba1cb74eb0ab
Vitalii Dylovyi <vitalii.dylovyi@gmail.com>
2025-03-19 01:45:09 +0200

    complete task

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const toggleOrderModal = () => {
   const orderModal = document.getElementById('order-modal');
   orderModal.classList.toggle('is-open');
 };
+
+const toggleNavMenu = () => {
+  const menuModal = document.getElementById('menu-modal');
+  menuModal.classList.toggle('is-open');
+};
